d8a89dcee6d3c6435d0ccdf58add9138c3e01ec1
kaiyikang <kaiyikang@gmail.com>
2025-09-03 19:18:47 +0200

    Update Resource class documentation for clarity

--- src/main/java/com/kaiyikang/winter/io/Resource.java
+++ src/main/java/com/kaiyikang/winter/io/Resource.java
@@ -1,8 +1,8 @@
 package com.kaiyikang.winter.io;
 
 /**
- * Path of the resource, base path for jar, full path for file.
- * Name of the resource, relative path for jar and file.
+ * Path: base path for jar, full path for file.
+ * Name: relative path for jar and file as identifier.
  */
 public record Resource(String path, String name) {
 
