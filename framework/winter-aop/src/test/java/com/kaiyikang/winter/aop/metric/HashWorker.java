2e9f3d762551e8b72efccc2188466b3754a54bd6
kaiyikang <kaiyikang@gmail.com>
2025-10-17 12:35:16 +0200

    Make sha1 method final in HashWorker to ensure proper AOP proxying and add @Component annotation to MetricProxyBeanPostProcessor

--- framework/winter-aop/src/test/java/com/kaiyikang/winter/aop/metric/HashWorker.java
+++ framework/winter-aop/src/test/java/com/kaiyikang/winter/aop/metric/HashWorker.java
@@ -5,8 +5,19 @@ import com.kaiyikang.winter.annotation.Component;
 @Component
 public class HashWorker extends BaseWorker {
 
+    /**
+     * AOP frameworks intercept method calls through subclass proxies (CGLIB).
+     * 
+     * The principle of subclass proxies is: They create a subclass of the target
+     * class and override the methods that need to be enhanced within this subclass.
+     * 
+     * However, final methods in Java cannot be overridden by subclasses.
+     * 
+     * Therefore, when a class contains final methods, the proxy framework cannot
+     * create a proxy class and will directly return the original class.
+     */
     @Metric("SHA-1")
-    public String sha1(String input) {
+    public final String sha1(String input) {
         return hash("SHA-1", input);
     }
 
--- framework/winter-aop/src/test/java/com/kaiyikang/winter/aop/metric/MetricProxyBeanPostProcessor.java
+++ framework/winter-aop/src/test/java/com/kaiyikang/winter/aop/metric/MetricProxyBeanPostProcessor.java
@@ -1,7 +1,9 @@
 package com.kaiyikang.winter.aop.metric;
 
+import com.kaiyikang.winter.annotation.Component;
 import com.kaiyikang.winter.aop.AnnotationProxyBeanPostProcessor;
 
+@Component
 public class MetricProxyBeanPostProcessor extends AnnotationProxyBeanPostProcessor<Metric> {
 
 }
