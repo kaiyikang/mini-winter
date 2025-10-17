3cd2a0e0f2d5f6228a3cc5fe3e47c3b206eda9eb
ValMobYKang <yikai.kang@valtech.com>
2025-10-17 10:20:34 +0200

    Add Around annotation for AOP invocation handling

---
+++ framework/winter-aop/src/main/java/com/kaiyikang/winter/annotation/Around.java
@@ -0,0 +1,20 @@
+package com.kaiyikang.winter.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface Around {
+
+    /**
+     * Bean name of Invocation handler
+     */
+    String value();
+}
