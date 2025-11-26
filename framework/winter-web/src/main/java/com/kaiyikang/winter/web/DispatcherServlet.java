d878d71655c27bf04b01eb40218e388a168ad56a
kaiyikang <kaiyikang@gmail.com>
2025-11-26 22:50:02 +0100

    Enhance DispatcherServlet to support controller registration and method validation

--- framework/winter-web/src/main/java/com/kaiyikang/winter/web/DispatcherServlet.java
+++ framework/winter-web/src/main/java/com/kaiyikang/winter/web/DispatcherServlet.java
@@ -2,11 +2,18 @@ package com.kaiyikang.winter.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.kaiyikang.winter.annotation.Controller;
+import com.kaiyikang.winter.annotation.RestController;
 import com.kaiyikang.winter.context.ApplicationContext;
+import com.kaiyikang.winter.context.ConfigurableApplicationContext;
 import com.kaiyikang.winter.io.PropertyResolver;
 
 import jakarta.servlet.ServletException;
@@ -19,14 +26,65 @@ public class DispatcherServlet extends HttpServlet {
     final Logger logger = LoggerFactory.getLogger(getClass());
 
     ApplicationContext applicationContext;
+    ViewResolver viewResolver;
+
+    String resourcePath;
+    String faviconPath;
+
+    List<Dispatcher> getDispatchers = new ArrayList<>();
+    List<Dispatcher> postDispatchers = new ArrayList<>();
 
     public DispatcherServlet(ApplicationContext applicationContext, PropertyResolver propertyResolver) {
         this.applicationContext = applicationContext;
+        this.viewResolver = applicationContext.getBean(ViewResolver.class);
+        this.resourcePath = propertyResolver.getProperty("${winter.web.static-path:/static/}");
+        this.faviconPath = propertyResolver.getProperty("${winter.web.favicon-path:/favicon.ico}");
+        if (!this.resourcePath.endsWith("/")) {
+            this.resourcePath = this.resourcePath + "/";
+        }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        logger.info("init {}.", getClass().getName());
+        // 从applicationContext中获取所有的beanDef
+        for (var def : ((ConfigurableApplicationContext) this.applicationContext).findBeanDefinitions(Object.class)) {
+            Class<?> beanClass = def.getBeanClass();
+            Object beanInstance = def.getRequiredInstance();
+            // 获取Annotation
+            Controller controller = beanClass.getAnnotation(Controller.class);
+            RestController restController = beanClass.getAnnotation(RestController.class);
+            if (controller != null && restController != null) {
+                throw new ServletException("Found @Controller and @RestController on class: " + beanClass.getName());
+            }
+            if (controller != null) {
+                addController(false, def.getName(), beanInstance);
+            }
+            if (restController != null) {
+                addController(true, def.getName(), beanInstance);
+            }
+        }
     }
 
     @Override
     public void destroy() {
         this.applicationContext.close();
+    }
+
+    void addController(boolean isRest, String name, Object instance) throws ServletException {
+        logger.info("add {} controller '{}' : {}", isRest ? "REST" : "MVC", name, instance.getClass().getName());
+        addMethods(isRest, name, instance, instance.getClass());
+    }
+
+    void addMethods(boolean isRest, String name, Object instance, Class<?> type) throws ServletException {
+    }
+
+    void checkMethod(Method m) throws ServletException {
+        int mod = m.getModifiers();
+        if (Modifier.isStatic(mod)) {
+            throw new ServletException("Cannot do URL mapping to a static method: " + m);
+        }
+        m.setAccessible(true);
     }
 
     @Override
@@ -35,5 +93,8 @@ public class DispatcherServlet extends HttpServlet {
         PrintWriter pw = resp.getWriter();
         pw.write("<h1>Hello, world!</h1>");
         pw.flush();
+    }
+
+    static class Dispatcher {
     }
 }
