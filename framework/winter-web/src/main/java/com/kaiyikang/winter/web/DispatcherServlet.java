e85b6cea133be89eb0681ce10ee00252883cb764
kaiyikang <kaiyikang@gmail.com>
2025-11-27 21:42:35 +0100

    Enhance DispatcherServlet to support method dispatching for GET and POST requests, including error handling and resource serving

--- framework/winter-web/src/main/java/com/kaiyikang/winter/web/DispatcherServlet.java
+++ framework/winter-web/src/main/java/com/kaiyikang/winter/web/DispatcherServlet.java
@@ -1,9 +1,12 @@
 package com.kaiyikang.winter.web;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +14,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.kaiyikang.winter.annotation.Controller;
+import com.kaiyikang.winter.annotation.GetMapping;
+import com.kaiyikang.winter.annotation.PostMapping;
 import com.kaiyikang.winter.annotation.RestController;
 import com.kaiyikang.winter.context.ApplicationContext;
 import com.kaiyikang.winter.context.ConfigurableApplicationContext;
+import com.kaiyikang.winter.exception.ErrorResponseException;
+import com.kaiyikang.winter.exception.NestedRuntimeException;
 import com.kaiyikang.winter.io.PropertyResolver;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -77,6 +86,24 @@ public class DispatcherServlet extends HttpServlet {
     }
 
     void addMethods(boolean isRest, String name, Object instance, Class<?> type) throws ServletException {
+        for (Method m : type.getDeclaredMethods()) {
+            GetMapping get = m.getAnnotation(GetMapping.class);
+            if (get != null) {
+                checkMethod(m);
+                // value 是@GetMapping中的路径
+                this.getDispatchers.add(new Dispatcher("GET", isRest, instance, m, get.value()));
+            }
+            PostMapping post = m.getAnnotation(PostMapping.class);
+            if (post != null) {
+                checkMethod(m);
+                this.postDispatchers.add(new Dispatcher("POST", isRest, instance, m, post.value()));
+            }
+        }
+        // 加入父类的方法
+        Class<?> superClass = type.getSuperclass();
+        if (superClass != null) {
+            addMethods(isRest, name, instance, superClass);
+        }
     }
 
     void checkMethod(Method m) throws ServletException {
@@ -89,12 +116,116 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("{} {}", req.getMethod(), req.getRequestURI());
-        PrintWriter pw = resp.getWriter();
-        pw.write("<h1>Hello, world!</h1>");
-        pw.flush();
+        String url = req.getRequestURI();
+        if (url.equals(this.faviconPath) || url.startsWith(this.resourcePath)) {
+            doResource(url, req, resp);
+        } else {
+            doService(req, resp, getDispatchers);
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doService(req, resp, postDispatchers);
+    }
+
+    /* 捕获所有的可能，并且转换成合适的HTTP错误响应 */
+    void doService(HttpServletRequest req, HttpServletResponse resp, List<Dispatcher> dispatchers)
+            throws ServletException, IOException {
+        String url = req.getRequestURI();
+        try {
+            doService(url, req, resp, dispatchers);
+        } catch (ErrorResponseException e) {
+            logger.warn("process request failed with status " + e.statusCode + " : " + url, e);
+            if (!resp.isCommitted()) {
+                resp.resetBuffer();
+                resp.sendError(e.statusCode);
+            }
+        } catch (RuntimeException | ServletException | IOException e) {
+            logger.warn("process request failed: " + url, e);
+            throw e;
+        } catch (Exception e) {
+            logger.warn("process request failed: " + url, e);
+            throw new NestedRuntimeException();
+        }
+    }
+
+    void doService(String url, HttpServletRequest req, HttpServletResponse resp, List<Dispatcher> dispatchers) {
+        for (Dispatcher dispatcher : dispatchers) {
+            Result result = dispatcher.process(url, req, resp);
+        }
+    }
+
+    /* 寻找/webapps/myapp/static/*的资源，并返回给浏览器 */
+    void doResource(String url, HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ServletContext ctx = req.getServletContext();
+        try (InputStream input = ctx.getResourceAsStream(url)) {
+            if (input == null) {
+                resp.sendError(404, "Not Found");
+                return;
+            }
+            // 获取文件名判断mime类型
+            String file = url;
+            int n = url.lastIndexOf('/');
+            if (n >= 0) {
+                file = url.substring(n + 1);
+            }
+            String mime = ctx.getMimeType(file);
+            if (mime == null) {
+                mime = "application/octect-stream";
+            }
+            resp.setContentType(mime);
+            ServletOutputStream output = resp.getOutputStream();
+            input.transferTo(output);
+            output.flush();
+        }
     }
 
     static class Dispatcher {
+
+        public Dispatcher(String httpMethod, boolean isRest, Object controller, Method method, String urlPattern)
+                throws ServletException {
+
+        }
+
+        Result process(String url, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        }
+
+        Object convertToType(Class<?> classType, String s) {
+
+        }
+
+        String getOrDefault(HttpServletRequest request, String name, String defaultValue) {
+
+        }
+    }
+
+    static enum ParamType {
+        PATH_VARIABLE, REQUEST_PARAM, REQUEST_BODY, SERVLET_VARIABLE;
+    }
+
+    static class Param {
+        String name;
+        ParamType paramType;
+        Class<?> classType;
+        String defaultValue;
+
+        public Param(String httpMethod, Method method, Parameter parameter, Annotation[] annotations)
+                throws ServletException {
+
+        }
+
+        @Override
+        public String toString() {
+            return "Param [name=" + name + ", paramType=" + paramType + ", classType=" + classType + ", defaultValue="
+                    + defaultValue + "]";
+        }
+    }
+
+    // 这里的static确保它不会持有 DispatcherServlet.this
+    static record Result(boolean processed, Object returnObject) {
     }
 }
