ced49a2921e5125933092f13ed553beafd46da97
kaiyikang <kaiyikang@gmail.com>
2025-12-06 21:40:40 +0100

    Add unit tests for DispatcherServlet initialization; set up MockServletContext

--- framework/winter-web/src/test/java/com/itranswarp/summer/web/DispatcherServletTest.java
+++ framework/winter-web/src/test/java/com/itranswarp/summer/web/DispatcherServletTest.java
@@ -1,5 +1,21 @@
 package com.itranswarp.summer.web;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.mock.web.MockServletContext;
+
+import com.kaiyikang.winter.web.DispatcherServlet;
+
+import jakarta.servlet.ServletException;
+
 public class DispatcherServletTest {
 
+    DispatcherServlet dispatcherServlet;
+    MockServletContext ctx;
+
+    @BeforeEach
+    void init() throws ServletException {
+        this.ctx = createMockServletContext();
+        https://liaoxuefeng.com/books/summerframework/web/mvc/index.html
+        https://github.com/michaelliao/summer-framework/blob/main/framework/summer-web/src/test/java/com/itranswarp/summer/web/DispatcherServletTest.java
+    }
 }
