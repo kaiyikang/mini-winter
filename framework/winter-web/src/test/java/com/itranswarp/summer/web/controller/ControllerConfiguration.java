4352f4e2a93c40a87c6aab0c50fd9a8d8fa8b70e
kaiyikang <kaiyikang@gmail.com>
2025-12-06 21:29:09 +0100

    Implement ApiController and MvcController with various API endpoints; add ControllerConfiguration for MVC setup

--- framework/winter-web/src/test/java/com/itranswarp/summer/web/controller/ControllerConfiguration.java
+++ framework/winter-web/src/test/java/com/itranswarp/summer/web/controller/ControllerConfiguration.java
@@ -1,6 +1,11 @@
 package com.itranswarp.summer.web.controller;
 
-//TODO
+import com.kaiyikang.winter.annotation.Configuration;
+import com.kaiyikang.winter.annotation.Import;
+import com.kaiyikang.winter.web.WebMvcConfiguration;
+
+@Configuration
+@Import(WebMvcConfiguration.class)
 public class ControllerConfiguration {
 
 }
